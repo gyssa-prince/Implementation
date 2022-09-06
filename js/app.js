1a46eca552aa20a0a90e5274bcc20bb1611102a6
GYSSAGARA Prince <mhzprnc@gmail.com>
2022-09-07 01:05:05 +0200

    Update

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+     slidesPerView: 1,
+    loop: true,
+
+     autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+     },
+     allowTouchMove: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  
+  });
+
+  console.log(mySwiper);
